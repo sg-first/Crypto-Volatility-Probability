2094abda114a7c70ff7ff246f9210fe3dbdd9e80
3A <524019351@qq.com>
2023-04-07 17:59:56 +0800

    加载数据

---
+++ main.py
@@ -0,0 +1,48 @@
+
+def isCoinName(line:str):
+    return line.find('/USDT')!=-1
+
+def isVolatility(line:str):
+    return line.find('%')!=-1
+
+def getCoinName(line:str):
+    last=0
+    first=0
+    cnt=0
+    for i in line:
+        if i==' ' and first==0:
+            first=cnt+1
+        cnt+=1
+    cnt=0
+    for i in line:
+        if i=='/':
+            last=cnt-1
+        cnt+=1
+    return line[first:last]
+
+def txtToDict(lines:list):
+    retDict = {}
+    i = 0
+    while i<len(lines):
+        line = lines[i]
+        if isCoinName(line):
+            coinName = getCoinName(line)
+            # 找涨幅
+            i += 1
+            while not isVolatility(lines[i]):
+                i += 1
+            volatilityStr = lines[i]
+            volatilityStr = volatilityStr.replace(' ', '')
+            volatility = float(volatilityStr.rstrip('%'))
+            retDict[coinName] = volatility
+        i += 1
+    return retDict
+
+start = 1
+end = 1
+allRetDict = []
+
+for i in range(start, end+1):
+    f = open(str(i)+'.txt', encoding="utf-8")
+    lines = f.read().split('\n')
+    allRetDict.append(txtToDict(lines))
