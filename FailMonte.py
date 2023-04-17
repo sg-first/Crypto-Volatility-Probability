2108cf81654f08e444dbae218418d70be01ab862
3A <524019351@qq.com>
2023-04-17 19:37:00 +0800

    亏损后追投计算

--- FailMonte.py
+++ FailMonte.py
@@ -1,11 +1,18 @@
-kuiRate = 1-0.495
-benjin = 150
-zhuitou = 150
+zhiying = 0.5
+zhisun = -0.8
+times = 25
+mubiao = 2500
+beilv = 75
+benjin = mubiao / (zhiying / 100 * beilv * times)
+kuiRate = 1 - (-zhisun/100 * beilv)
+print(benjin, kuiRate)
+zhuitou = benjin
 
-for i in range(2,31):
+for i in range(2, times + 1):
     benjin *= kuiRate
-    shengyuDay = 31-i
-    needBenjin = 2500/(0.00666*75*shengyuDay)
+    shengyuDay = times + 1 - i
+    needBenjin = mubiao / (zhiying / 100 * beilv * shengyuDay)
+    print(i, needBenjin, 'needMoney')
     zhuitou += needBenjin - benjin
-    print(i, zhuitou)
+    # print(i, zhuitou, 'zhuitou')
     benjin = needBenjin
