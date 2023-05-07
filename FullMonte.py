c6b86198f0962c634a2147771e925ef746c4c7f0
3A <524019351@qq.com>
2023-05-07 23:16:47 +0800

    把目标和最大投入金额当参数算胜率

--- FullMonte.py
+++ FullMonte.py
@@ -1,6 +1,6 @@
 import random
 
-times = 25
+times = 730
 zhiying = 0.5
 zhisun = -0.8
 zhiyingDistance = None
@@ -15,41 +15,45 @@ reset()
 print(rate, rate + 100)
 
 beilv = 75
-mubiao = 2500
+mubiao = 1200
+maxTouru = 70
 
 def test():
     benjin = mubiao / (zhiying / 100 * beilv * times)
-    # print('本金', benjin)
+    print('本金', benjin)
     zhuitou = benjin
 
-    for i in range(1,times+1):
-        if random.randint(0, int(rate) + 100) < rate:
-            addMoney = zhiying/100 * beilv * benjin
-            benjin += addMoney
-            shouyi = benjin - zhuitou
-            if shouyi >= mubiao:
-                # print('SUCCESS', i, 'days. Need money', zhuitou, '剩余', benjin, '收益', shouyi)
-                return True, shouyi
-        else:
-            addMoney = zhisun/100 * beilv * benjin
-            benjin += addMoney
-            # 失败之后追投
-            shengyuDay = times + 1 - i
-            if shengyuDay == 0:
-                break
+    for i in range(1, times+1):
+        for _ in range(4):
+            if random.randint(0, int(rate) + 100) < rate:
+                addMoney = zhiying/100 * beilv * benjin
+                benjin += addMoney
+                shouyi = benjin - zhuitou
+                # print('WIN', benjin, shouyi)
+                if shouyi >= mubiao:
+                    print('SUCCESS', i, 'days. Need money', zhuitou, '剩余', benjin, '收益', shouyi)
+                    return True, shouyi
             else:
-                needBenjin = mubiao / (zhiying / 100 * beilv * shengyuDay)
-                delta = needBenjin - benjin
-                if delta > 0:
-                    zhuitou += delta
-                    benjin = needBenjin
-                    if (zhuitou - benjin) >= mubiao:
-                        shouyi = benjin - zhuitou
-                        # print('FAIL', i, 'days. Need money', zhuitou, '剩余', benjin, '收益', shouyi)
-                        return False, shouyi
+                addMoney = zhisun/100 * beilv * benjin
+                benjin += addMoney
+                # 失败之后追投
+                shengyuDay = times + 1 - i
+                if shengyuDay == 0:
+                    break
+                else:
+                    needBenjin = mubiao / (zhiying / 100 * beilv * shengyuDay)
+                    delta = needBenjin - benjin
+                    if delta > 0:
+                        zhuitou += delta
+                        benjin = needBenjin
+                        # print('FAIL', benjin, benjin - zhuitou)
+                        if (zhuitou - benjin) >= maxTouru:
+                            shouyi = benjin - zhuitou
+                            print('FAIL', i, 'days. Need money', zhuitou, '剩余', benjin, '收益', shouyi)
+                            return False, shouyi
 
     shouyi = benjin - zhuitou
-    # print('FAIL. Need money', zhuitou, '剩余', benjin, '收益', shouyi)
+    print('FAIL. Need money', zhuitou, '剩余', benjin, '收益', shouyi)
     return False, shouyi
 
 def expectations():
@@ -67,8 +71,8 @@ def expectations():
     exp = sum(allShouyi) / len(allShouyi)
     print('>>>>胜率', rate, exp, 'in', zhiying, zhisun)
     return rate, exp
-# expectations()
-
+expectations()
+'''
 zhiying = 0.1
 zhisun = -0.8
 bestZhiying = None
@@ -88,4 +92,5 @@ while zhiying < 1:
     zhiying += 0.1
 
 print(bestZhiying, bestZhisun, bestRate)
+'''
 
